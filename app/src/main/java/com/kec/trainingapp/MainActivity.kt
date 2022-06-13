f3687d18203519a40b5f70855e94df964b16dd1c
unknown <emailtosujan@gmail.com>
2022-06-13 14:03:02 +0545

    using view binding

--- app/src/main/java/com/kec/trainingapp/MainActivity.kt
+++ app/src/main/java/com/kec/trainingapp/MainActivity.kt
@@ -4,14 +4,31 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.kec.trainingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    var binding: ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+        binding!!.loginBtn?.setOnClickListener {
+            // Get username
+            val username = binding!!.usernameEt?.text.toString().trim()
+
+            // Get password
+
+            // Validation logic
+            if (username.isEmpty()) {
+                Log.d("MainActivity", "username is empty")
+            } else {
+                Toast.makeText(this, "Logging in", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onStart() {
